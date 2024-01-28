295fc2c890509bd59897fb0cda4ab7d5ac0e3f00
FireStreaker2 <michaelccc2019@outlook.com>
2024-01-27 21:33:10 -0800

    migrate to gtts

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ from characterai import PyCAI
 from pydub import AudioSegment
 from pydub.playback import play
 from dotenv import load_dotenv
+from gtts import gTTS
 
 load_dotenv()
 
 config = {
     "caiToken": os.getenv("CAI_TOKEN"),
     "caiCharacter": os.getenv("CAI_CHARACTER"),
-    "ttsCharacter": os.getenv("TTS_CHARACTER"),
-    "ttsAPIKey": os.getenv("TTS_API_KEY"),
 }
 
 client = PyCAI(config["caiToken"])
@@ -21,8 +20,13 @@ chat = client.chat.get_chat(config["caiCharacter"])
 participants = chat["participants"]
 if not participants[0]["is_human"]:
     tgt = participants[0]["user"]["username"]
+    bot = participants[0]["name"]
+
 else:
     tgt = participants[1]["user"]["username"]
+    bot = participants[1]["name"]
+
+print(f"Now chatting with: {bot}")
 
 try:
     while True:
@@ -35,33 +39,15 @@ try:
 
         print(f"{name}: {text}")
 
-        response = requests.post(
-            "https://api.topmediai.com/v1/text2speech",
-            json={
-                "text": text,
-                "speaker": config["ttsCharacter"],
-                "emotion": "Neutral",
-            },
-            headers={
-                "accept": "application/json",
-                "x-api-key": config["ttsAPIKey"],
-                "Content-Type": "application/json",
-            },
-        )
+        tts = gTTS(text, lang="en")
+        tts.save("out.mp3")
 
-        if response.status_code == 200:
-            data = response.json()
-            url = requests.get(data["data"]["oss_url"])
-
-            with open("out.wav", "wb") as file:
-                file.write(url.content)
-
-            audio = AudioSegment.from_file("out.wav", format="mp3")
-            play(audio)
-
-        else:
-            print("Error generating TTS")
+        audio = AudioSegment.from_file("out.mp3", format="mp3")
+        play(audio)
 
 
 except KeyboardInterrupt:
-    os.remove("out.wav")
+    try:
+        os.remove("out.mp3")
+    except Exception:
+        pass
