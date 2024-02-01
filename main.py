b17acc722fc7be1271596046cc0a983e7ba99858
FireStreaker2 <michaelccc2019@outlook.com>
2024-01-31 18:59:09 -0800

    add openai support

--- main.py
+++ main.py
@@ -6,25 +6,37 @@ from pydub import AudioSegment
 from pydub.playback import play
 from dotenv import load_dotenv
 from gtts import gTTS
+from openai import OpenAI
 
 load_dotenv()
 
 config = {
     "caiToken": os.getenv("CAI_TOKEN"),
     "caiCharacter": os.getenv("CAI_CHARACTER"),
+    "openAIToken": os.getenv("OPENAI_TOKEN"),
+    "openAIModel": "gpt-3.5-turbo",
+    "aiMethod": "CAI",
 }
 
-client = PyCAI(config["caiToken"])
-chat = client.chat.get_chat(config["caiCharacter"])
+if config["aiMethod"] == "CAI":
+    client = PyCAI(config["caiToken"])
+    chat = client.chat.get_chat(config["caiCharacter"])
 
-participants = chat["participants"]
-if not participants[0]["is_human"]:
-    tgt = participants[0]["user"]["username"]
-    bot = participants[0]["name"]
+    participants = chat["participants"]
+    if not participants[0]["is_human"]:
+        tgt = participants[0]["user"]["username"]
+        bot = participants[0]["name"]
+
+    else:
+        tgt = participants[1]["user"]["username"]
+        bot = participants[1]["name"]
+
 
 else:
-    tgt = participants[1]["user"]["username"]
-    bot = participants[1]["name"]
+    client = OpenAI(api_key=config["openAIToken"])
+    history = []
+
+    bot = config["openAIModel"]
 
 print(f"Now chatting with: {bot}")
 
@@ -32,10 +44,24 @@ try:
     while True:
         message = input("You: ")
 
-        data = client.chat.send_message(chat["external_id"], tgt, message)
+        if config["aiMethod"] == "CAI":
+            data = client.chat.send_message(chat["external_id"], tgt, message)
 
-        name = data["src_char"]["participant"]["name"]
-        text = data["replies"][0]["text"]
+            name = data["src_char"]["participant"]["name"]
+            text = data["replies"][0]["text"]
+
+        else:
+            history.append({"role": "user", "content": message})
+
+            response = client.chat.completions.create(
+                model=config["openAIModel"],
+                messages=history,
+            )
+
+            name = config["openAIModel"]
+            text = response["choices"][0]["message"]["content"]
+
+            history.append({"role": "assistant", "content": text})
 
         print(f"{name}: {text}")
 
